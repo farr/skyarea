79b4ed95a555546fc797b886620ac434290e1ad4
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-11-04 14:32:32 +0000

    Added make_search_map.py to construct the search map.

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='skyarea',
     packages=['sky_area'],
-    scripts=['bin/process_areas.py', 'bin/run_sky_area.py']
+    scripts=['bin/make_search_map.py', 'bin/process_areas.py', 'bin/run_sky_area.py']
 )
