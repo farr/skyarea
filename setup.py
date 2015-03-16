0acf3525672b92a41dee7d4c0661b0db93ba40af
Will Meierjurgen Farr <wmfarr@gmail.com>
2015-03-16 15:33:38 -0700

    Changes to setup in prep for submission to PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='skyarea',
     packages=['sky_area'],
-    scripts=['bin/make_search_map.py', 'bin/process_areas.py', 'bin/run_sky_area.py']
+    scripts=['bin/make_search_map.py', 'bin/process_areas.py', 'bin/run_sky_area.py'],
+    version='1.0',
+    description='Code for computing credible regions on the sky from RA-DEC MCMC samples.',
+    author='Will M. Farr',
+    author_email='will.farr@ligo.org',
+    url='http://farr.github.io/skyarea/',
+    license='MIT',
+    keywords='MCMC credible regions skymap LIGO',
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering :: Astronomy',
+                 'Topic :: Scientific/Engineering :: Visualization']
 )
